d8d0d977e077989e72e6a4c29a2d85633f121545
sanghis96 <sanghi.samarth@gmail.com>
2016-06-08 01:05:24 +0530

    Utopian Tree solved

---
+++ competitiveProgramming/Hackerrank/UtopianTree.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0 ; i<t ; i++)
+        {
+            int height = 1;
+            int n = sc.nextInt();
+            for(int j=0 ; j<n ; j++)
+            {
+                if(j%2 == 0)
+                    height *= 2;
+                else
+                    height++;
+            }
+            System.out.println(height);
+        }
+    }
+}
