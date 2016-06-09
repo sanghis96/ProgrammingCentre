6013cb892539c918df1b1aa65efb44f3235e88fa
sanghis96 <sanghi.samarth@gmail.com>
2016-06-09 20:20:40 +0530

    Modified Kaprekar Numbers added

---
+++ competitiveProgramming/Hackerrank/ModifiedKaprekarNumbers.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        long p = sc.nextInt();
+        long q = sc.nextInt();
+        int flag = 0;
+        for(long i=p ; i<=q ; i++)
+            if(isKaprekar(i))
+            {    
+               System.out.print(i + " ");
+               flag = 1;
+            }
+        if(flag == 0)
+           System.out.println("INVALID RANGE");
+    }
+    static boolean isKaprekar(long n)
+    {
+        long square = n*n;
+        int d = 0;
+        long num = n;
+        while(num>0)
+        {
+            num /= 10;
+            d++;
+        }
+        long r=0;
+        int a=1;
+        for(int i=0 ; i<d ; i++)
+        {
+            r += square%10 * a;
+            square /= 10;
+            a *= 10;
+        }
+        long l = square;
+        if(l + r == n)
+            return true;
+        else
+            return false;
+    }
+}
