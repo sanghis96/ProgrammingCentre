7054666f974a4f8930def441656bc2af6e524ab1
sanghis96 <sanghi.samarth@gmail.com>
2016-06-09 18:02:38 +0530

    Lisa's Workbook added

---
+++ competitiveProgramming/Hackerrank/Lisa'sWorkbook.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] t = new int[n]; 
+        for(int i=0; i<n; i++)
+            t[i] = sc.nextInt();
+        int pageNo = 0;
+        int noOfQues = 0;
+        int specialProblem = 0;
+        int chapterNo = 1;
+        while(chapterNo<=n)
+        {
+            pageNo++;
+            for(int j=0; j<k; j++)
+            {
+                noOfQues++;
+                if(noOfQues == pageNo)
+                    specialProblem++;
+                if(noOfQues == t[chapterNo-1])
+                {
+                    chapterNo++;
+                    noOfQues = 0;
+                    break;
+                }
+            }
+        }
+        System.out.println(specialProblem);
+    }
+}
