c0bb9188b9b02de75f5bc8e6ecd8f079d1e69022
Samarth Sanghi <sanghi.samarth@gmail.com>
2016-06-11 09:40:47 +0530

    Class vs. Instance

--- competitiveProgramming/Hackerrank/30DaysHR/Day4.java
+++ competitiveProgramming/Hackerrank/30DaysHR/Day4.java
@@ -3,35 +3,32 @@ import java.util.*;
 
 public class Person 
 {
-    private int age;	
-  
-	public Person(int initialAge) 
-  {
-  		if(initialAge >= 0)
+    private int age;
+    public Person(int initialAge)
+    {
+    	if(initialAge >= 0)
             age = initialAge;
         else
         {
             System.out.println("Age is not valid, setting age to 0.");
             age = 0;
         }
-	}
-
-	public void amIOld() 
-  {
+    }
+    public void amIOld()
+    {
         System.out.print("You are ");
         if(age < 13)
             System.out.println("young.");
         else if(age < 18)
             System.out.println("a teenager.");
             else 
-               System.out.println("old.");
-	}
-
-	public void yearPasses() 
-  {	age++;  }
-  
-  public static void main(String[] args)
-  {
+                System.out.println("old.");
+    }
+    public void yearPasses()
+    {	age++;  }
+    
+    public static void main(String[] args)
+    {
        Scanner sc = new Scanner(System.in);
        int T = sc.nextInt();
        for(int i=0; i<T; i++)
