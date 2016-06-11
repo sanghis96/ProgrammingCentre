546c5f52016e9c4bf9b1ec9f1100f7a0c3062468
Samarth Sanghi <sanghi.samarth@gmail.com>
2016-06-11 23:45:32 +0530

    ACM ICPC Team solved

---
+++ competitiveProgramming/Hackerrank/ACM_ICPC_Team.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        String s = "";
+        for(int i=0; i<n; i++)
+        {   
+            s = sc.next();
+            for(int j=0; j<m; j++)
+                arr[i][j] = Integer.parseInt(s.charAt(j)+"");
+        }
+        int maxNoOfTopic=0, noOfTeams=0, count=0;
+        int[] topics = new int[m+1];
+        for(int i=0; i<=m; i++)
+            topics[i] = 0; 
+        for(int i=0; i<n-1; i++)
+            for(int j=i+1; j<n; j++)
+            {
+                count = 0;
+                for(int k=0; k<m; k++)
+                    if(arr[i][k]==1 || arr[j][k]==1)
+                        count++;
+                topics[count]++;
+                maxNoOfTopic = count>maxNoOfTopic?count:maxNoOfTopic;
+            }
+        System.out.println(maxNoOfTopic + "\n" + topics[maxNoOfTopic]);
+    }
+}
