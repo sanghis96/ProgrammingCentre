81c8a8238d7e50c69eaaf599fd4753f74a27ea0d
sanghis96 <sanghi.samarth@gmail.com>
2016-10-15 21:22:00 +0530

    3-D

--- opengl/prac8/prac8/code.cpp
+++ opengl/prac8/prac8/code.cpp
@@ -3,144 +3,90 @@
 
 void threeD(void)
 {
-/*	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
-   glMatrixMode(GL_MODELVIEW); 
-//glutWireSphere(5,5,15);
-//glutWireCone(2,3,15,15);
- glBegin(GL_POLYGON);                
-      // Top face 
-      
-      glColor3f(0.0f, 1.0f, 0.0f);     // Green
-      glVertex3f( 1.0f, 1.0f, -1.0f);
-      glVertex3f(-1.0f, 1.0f, -1.0f);
-      glVertex3f(-1.0f, 1.0f,  1.0f);
-      glVertex3f( 1.0f, 1.0f,  1.0f);
- glEnd();
-  glBegin(GL_POLYGON);   
-      // Bottom face 
-      glColor3f(1.0f, 0.5f, 0.0f);     // Orange
-      glVertex3f( 1.0f, -1.0f,  1.0f);
-      glVertex3f(-1.0f, -1.0f,  1.0f);
-      glVertex3f(-1.0f, -1.0f, -1.0f);
-      glVertex3f( 1.0f, -1.0f, -1.0f);
-       glEnd();
-       glBegin(GL_POLYGON);   
-      // Front face  
-      glColor3f(1.0f, 0.0f, 0.0f);     // Red
-      glVertex3f( 1.0f,  1.0f, 1.0f);
-      glVertex3f(-1.0f,  1.0f, 1.0f);
-      glVertex3f(-1.0f, -1.0f, 1.0f);
-      glVertex3f( 1.0f, -1.0f, 1.0f);
-       glEnd();
-  glBegin(GL_POLYGON);   
-      // Back face 
-      glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
-      glVertex3f( 1.0f, -1.0f, -1.0f);
-      glVertex3f(-1.0f, -1.0f, -1.0f);
-      glVertex3f(-1.0f,  1.0f, -1.0f);
-      glVertex3f( 1.0f,  1.0f, -1.0f);
-       glEnd();
-  glBegin(GL_POLYGON);   
-      // Left face 
-      glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-      glVertex3f(-1.0f,  1.0f,  1.0f);
-      glVertex3f(-1.0f,  1.0f, -1.0f);
-      glVertex3f(-1.0f, -1.0f, -1.0f);
-      glVertex3f(-1.0f, -1.0f,  1.0f);
-       glEnd();
-  glBegin(GL_POLYGON);   
-      // Right face 
-      glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
-      glVertex3f(1.0f,  1.0f, -1.0f);
-      glVertex3f(1.0f,  1.0f,  1.0f);
-      glVertex3f(1.0f, -1.0f,  1.0f);
-      glVertex3f(1.0f, -1.0f, -1.0f);
-       glEnd();
-glFlush();//Forces Execution
-*/
-
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
-   glMatrixMode(GL_MODELVIEW); 
-//glutWireSphere(5,5,15);
-//glutWireCone(2,3,15,15);
- glBegin(GL_POLYGON);                
-      // Top face 
+	glMatrixMode(GL_MODELVIEW); 
+	//glutWireSphere(5,5,15);
+	//glutWireCone(2,3,15,15);
+	glBegin(GL_POLYGON);                
       
+		// Top face 
       glColor3f(0.0f, 1.0f, 0.0f);     // Green
       glVertex3f( 1.0f, 1.0f, -1.0f);
       glVertex3f(-1.0f, 1.0f, -1.0f);
       glVertex3f(-1.0f, 1.0f,  1.0f);
       glVertex3f( 1.0f, 1.0f,  1.0f);
- glEnd();
-  glBegin(GL_POLYGON);   
-      // Bottom face 
+	  glEnd();
+	  glBegin(GL_POLYGON);   
+      
+	  // Bottom face 
       glColor3f(1.0f, 0.5f, 0.0f);     // Orange
       glVertex3f( 1.0f, -1.0f,  1.0f);
       glVertex3f(-1.0f, -1.0f,  1.0f);
       glVertex3f(-1.0f, -1.0f, -1.0f);
       glVertex3f( 1.0f, -1.0f, -1.0f);
-       glEnd();
-       glBegin(GL_POLYGON);   
-      // Front face  
+      glEnd();
+      glBegin(GL_POLYGON);   
+      
+	  // Front face  
       glColor3f(1.0f, 0.0f, 0.0f);     // Red
       glVertex3f( 1.0f,  1.0f, 1.0f);
       glVertex3f(-1.0f,  1.0f, 1.0f);
       glVertex3f(-1.0f, -1.0f, 1.0f);
       glVertex3f( 1.0f, -1.0f, 1.0f);
-       glEnd();
-  glBegin(GL_POLYGON);   
-      // Back face 
+      glEnd();
+	  glBegin(GL_POLYGON);   
+      
+	  // Back face 
       glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
       glVertex3f( 1.0f, -1.0f, -1.0f);
       glVertex3f(-1.0f, -1.0f, -1.0f);
       glVertex3f(-1.0f,  1.0f, -1.0f);
       glVertex3f( 1.0f,  1.0f, -1.0f);
-       glEnd();
-  glBegin(GL_POLYGON);   
-      // Left face 
+      glEnd();
+	  glBegin(GL_POLYGON);   
+      
+	  // Left face 
       glColor3f(0.0f, 0.0f, 1.0f);     // Blue
       glVertex3f(-1.0f,  1.0f,  1.0f);
       glVertex3f(-1.0f,  1.0f, -1.0f);
       glVertex3f(-1.0f, -1.0f, -1.0f);
       glVertex3f(-1.0f, -1.0f,  1.0f);
-       glEnd();
-  glBegin(GL_POLYGON);   
-      // Right face 
+      glEnd();
+	  glBegin(GL_POLYGON);   
+      
+	  // Right face 
       glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
       glVertex3f(1.0f,  1.0f, -1.0f);
       glVertex3f(1.0f,  1.0f,  1.0f);
       glVertex3f(1.0f, -1.0f,  1.0f);
       glVertex3f(1.0f, -1.0f, -1.0f);
-       glEnd();
-glFlush();//Forces Execution
-	/*
-	glColor3f(0.0,0.0,1.0); // blue.
-	glutSolidCone(3,4,100,15);
-	glFlush();//Forces Execution	*/
+      glEnd();
+	  glFlush();//Forces Execution
 }
-
 
 void Init()
 { 
-     /*glClearColor(1.0,1.0,1.0,0); 
+    glClearColor(1.0,1.0,1.0,0); 
     glColor3f(1.0,0.0,0.0); // red
     glViewport(0 , 0 , 640 , 480);// Set the vieport for viewing with lower left corner & Width & height
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-   glOrtho(-10,10,-10,10,-10,10);
-   glRotatef(135.0,1.0,1.0,0.0);*/
-
-	glClearColor(1.0,1.0,1.0,0); 
-    glColor3f(1.0,0.0,0.0); // red
-    glViewport(0 , 0 , 640 , 480);// Set the vieport for viewing with lower left corner & Width & height
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(160.0,1.0,1.0, 0.0);
-    gluLookAt(0.0, 0.0, 3.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0);
-   glRotatef(150.0,1.0,1.0,1.0);
+	int ch;
+    printf("Menu:-\n1.Ortho\n2.Perspective\nEnter your choice:");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+		case 1:
+			glOrtho(-10,10,-10,10,-10,10);
+			glRotatef(120.0,1.0,0.0,0.0);
+			break;
+		case 2:
+			gluPerspective(80.0,1.0,1.0, 0.0);
+			gluLookAt(1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0);
+			glRotatef(150.0,1.0,1.0,1.0);
+			break;
+	}
 }
-
-
 
 int main(int argc, char **argv)
 {
