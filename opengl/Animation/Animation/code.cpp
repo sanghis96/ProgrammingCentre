1585c88bda3f0f28de7629913060182b66d36b1d
sanghis96 <sanghi.samarth@gmail.com>
2016-10-27 03:24:05 +0530

    Disco Ball

--- opengl/Animation/Animation/code.cpp
+++ opengl/Animation/Animation/code.cpp
@@ -1,155 +1,139 @@
-#include <GL/glut.h>    // Header File For The GLUT Library 
-#include <GL/gl.h>    // Header File For The OpenGL32 Library
-#include <GL/glu.h>    // Header File For The GLu32 Library
-//#include <unistd.h>     // Header File For sleeping.
-
-/* ASCII code for the escape key. */
-#define ESCAPE 27
-
-/* The number of our GLUT window */
-int window; 
-
-/* rotation angle for the triangle. */
-float rtri = 0.0f;
-
-/* rotation angle for the quadrilateral. */
-float rquad = 0.0f;
-
-/* A general OpenGL initialization function.  Sets all of the initial parameters. */
-// We call this right after our OpenGL window is created.
-void InitGL(int Width, int Height)         
+#include<windows.h>
+#ifdef __APPLE__
+#else
+#include <GL/glut.h>
+#endif
+#include<math.h>
+#include <stdlib.h>
+int i=0;
+int s=1;
+static int slices = 16;
+static int stacks = 16;
+static void resize(int width, int height)
 {
-  // This Will Clear The Background Color To Black
-  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);     
-  glClearDepth(1.0);                // Enables Clearing Of The Depth Buffer
-  glDepthFunc(GL_LESS);                // The Type Of Depth Test To Do
-  glEnable(GL_DEPTH_TEST);            // Enables Depth Testing
-  glShadeModel(GL_SMOOTH);            // Enables Smooth Color Shading
-
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();                // Reset The Projection Matrix
-
-  gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,100.0f);  
-
-  glMatrixMode(GL_MODELVIEW);
+    const float ar = (float) width / (float) height;
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity() ;
 }
-
-/* The function called when our window is resized (which shouldn't happen, because we're fullscreen) */
-void ReSizeGLScene(int Width, int Height)
+static void display(void)
 {
-  if (Height==0)                // Prevent A Divide By Zero If The Window Is Too Small
-    Height=1;
+    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
+    const double a = t*90.0;
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3d(1,1,1);
+    glPushMatrix();
+        glTranslated(0,0,-6);
+        glRotated(60,1,0,0);
+        glRotated(a,0,0,1);
+        switch (i) {
+	case 0 :
+		glDisable(GL_LIGHT0);
+		glColor3f(1.0,0.0,0.0);
+		i = (i + 1) % 4;
+		break;
+	case 1:
+		glEnable(GL_LIGHT0);
+		glColor3f(0.0,0.0,1.0);
+		i = (i + 1) % 4;
+		break;
 
-  glViewport(0, 0, Width, Height);        // Reset The Current Viewport And Perspective Transformation
-
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-
-  gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,100.0f);
-  glMatrixMode(GL_MODELVIEW);
-}
-
-float ballX = -0.5f;
-float ballY = 0.0f;
-float ballZ = 0.0f;
-
-void drawBall(void) {
-        glColor3f(0.0, 1.0, 0.0); //set ball colour
-        glTranslatef(ballX,ballY,ballZ); //moving it toward the screen a bit on creation
-        //glRotatef(ballX,ballX,ballY,ballZ);
-        glutSolidSphere (0.3, 20, 20); //create ball.
-        glTranslatef(ballX+1.5,ballY,ballZ); //moving it toward the screen a bit on creation
-        glutSolidSphere (0.3, 20, 20); //
-        }
-
-
-/* The main drawing function. */
-void DrawGLScene()
-{
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);        // Clear The Screen And The Depth Buffer
-  glLoadIdentity();                // Reset The View
-
-  glTranslatef(rtri,0.0f,-6.0f);        // Move Left 1.5 Units And Into The Screen 6.0
-   
-  //glRotatef(rtri,1.0f,0.0f,0.0f);        // Rotate The Triangle On The Y axis
-  // draw a triangle (in smooth coloring mode)
-  glBegin(GL_POLYGON);                // start drawing a polygon
-  glColor3f(1.0f,0.0f,0.0f);            // Set The Color To Red
-  glVertex3f(-1.0f, 1.0f, 0.0f);        // Top left
-  glVertex3f(0.4f, 1.0f, 0.0f);
-  
-  glVertex3f(1.0f, 0.4f, 0.0f);
-  
-  glColor3f(0.0f,1.0f,0.0f);            // Set The Color To Green
-  glVertex3f( 1.0f,0.0f, 0.0f);        // Bottom Right
-  glColor3f(0.0f,0.0f,1.0f);            // Set The Color To Blue
-  glVertex3f(-1.0f,0.0f, 0.0f);// Bottom Left    
-
-  //glVertex3f();
-  glEnd();                    // we're done with the polygon (smooth color interpolation)
-  drawBall();
- 
-  rtri+=0.005f;                    // Increase The Rotation Variable For The Triangle
-  if(rtri>2)
-      rtri=-2.0f;
-  rquad-=15.0f;                    // Decrease The Rotation Variable For The Quad
-
-  // swap the buffers to display, since double buffering is used.
+	case 2:
+		glDisable(GL_LIGHT1);
+		glColor3f(1.0,1.0,0.0);
+		i = (i + 1) % 4;
+		break;
+	case 3:
+		glEnable(GL_LIGHT1);
+		glColor3f(0.0,1.0,1.0);
+		i = (i + 1) % 4;
+		break;
+	case 27: //Escape key
+		exit(0);
+	default:
+		break;
+	}
+		
+        glutSolidSphere(1,slices,stacks);
+    glPopMatrix();
   glutSwapBuffers();
 }
-
-/* The function called whenever a key is pressed. */
-void keyPressed(unsigned char key, int x, int y) 
+static void key(unsigned char key, int x, int y)
 {
-    /* sleep to avoid thrashing this procedure */
-   // usleep(100);
-
-    /* If escape is pressed, kill everything. */
-    if (key == ESCAPE) 
-    { 
-    /* shut down our window */
-    glutDestroyWindow(window);
-   
-    /* exit the program...normal termination. */
-    exit(0);                  
+    switch (key)
+    {
+        case 27 :
+        case 'q':
+            exit(0);
+            break;
+        case '+':
+            slices++;
+            stacks++;
+            break;
+        case '-':
+            if (slices>3 && stacks>3)
+            {
+                slices--;
+                stacks--;
+            }
+            break;
     }
+    glutPostRedisplay();
 }
+static void idle(void)
+{
+    glutPostRedisplay();
+}
+const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
+const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
+const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f };
 
-int main(int argc, char **argv) 
-{  
-  glutInit(&argc, argv);  
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("Animation (Spinnig Sphere with Disco Lighting)");
 
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);  
+    glutReshapeFunc(resize);
+    glutDisplayFunc(display);
+    glutKeyboardFunc(key);
+    glutIdleFunc(idle);
 
-  /* get a 640 x 480 window */
-  glutInitWindowSize(640, 480);  
+    glClearColor(0,0,0,0);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
 
-  /* the window starts at the upper left corner of the screen */
-  glutInitWindowPosition(0, 0);  
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
 
-  /* Open a window */  
-  window = glutCreateWindow("Moving Car");  
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_FLAT);
+    glShadeModel(GL_FLAT);
 
-  /* Register the function to do all our OpenGL drawing. */
-  glutDisplayFunc(&DrawGLScene);  
+    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
-  /* Go fullscreen.  This is as soon as possible. */
-  //glutFullScreen();
+    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
 
-  /* Even if there are no events, redraw our gl scene. */
-  glutIdleFunc(&DrawGLScene);
+    glutMainLoop();
 
-  /* Register the function called when our window is resized. */
-  glutReshapeFunc(&ReSizeGLScene);
-
-  /* Register the function called when the keyboard is pressed. */
-  glutKeyboardFunc(&keyPressed);
-
-  /* Initialize our window. */
-  InitGL(640, 480);
-  
-  /* Start Event Processing Engine */  
-  glutMainLoop();  
-
-  return 1;
+    return EXIT_SUCCESS;
 }
