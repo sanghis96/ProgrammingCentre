1402dea3855bbafdf2af9aae4108c1916152cb2c
sanghis96 <sanghi.samarth@gmail.com>
2016-10-27 13:23:57 +0530

    executed moving car

--- opengl/Animation/Animation/code.cpp
+++ opengl/Animation/Animation/code.cpp
@@ -1,139 +1,89 @@
-#include<windows.h>
-#ifdef __APPLE__
-#else
 #include <GL/glut.h>
-#endif
-#include<math.h>
-#include <stdlib.h>
-int i=0;
-int s=1;
-static int slices = 16;
-static int stacks = 16;
-static void resize(int width, int height)
-{
-    const float ar = (float) width / (float) height;
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity() ;
-}
-static void display(void)
-{
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,1,1);
-    glPushMatrix();
-        glTranslated(0,0,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        switch (i) {
-	case 0 :
-		glDisable(GL_LIGHT0);
-		glColor3f(1.0,0.0,0.0);
-		i = (i + 1) % 4;
-		break;
-	case 1:
-		glEnable(GL_LIGHT0);
-		glColor3f(0.0,0.0,1.0);
-		i = (i + 1) % 4;
-		break;
+#include <GL/gl.h>
+#include <GL/glu.h>
 
-	case 2:
-		glDisable(GL_LIGHT1);
-		glColor3f(1.0,1.0,0.0);
-		i = (i + 1) % 4;
-		break;
-	case 3:
-		glEnable(GL_LIGHT1);
-		glColor3f(0.0,1.0,1.0);
-		i = (i + 1) % 4;
-		break;
-	case 27: //Escape key
-		exit(0);
-	default:
-		break;
-	}
-		
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
+int window;
+
+float rtri = 0.0f;
+float rquad = 0.0f;
+
+float ballX = -0.5f;
+float ballY = 0.0f;
+float ballZ = 0.0f;
+
+void drawBall(void) 
+{
+        glColor3f(1.0, 1.0, 0.0);
+        glTranslatef(ballX,ballY,ballZ);
+        glutSolidSphere (0.3, 200, 200);
+        glTranslatef(ballX+1.5,ballY,ballZ);
+        glutSolidSphere (0.3, 200, 200);
+}
+
+void DrawGLScene()
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glLoadIdentity();
+
+  glTranslatef(rtri,0.0f,-6.0f);
+  
+  glBegin(GL_POLYGON);
+  glColor3f(1.0f,0.0f,0.0f);
+  glVertex3f(-1.0f, 1.0f, 0.0f);
+  glVertex3f(0.4f, 1.0f, 0.0f);
+  glVertex3f(1.0f, 0.4f, 0.0f);
+  glVertex3f( 1.0f,0.0f, 0.0f);
+  glVertex3f(-1.0f,0.0f, 0.0f); 
+
+  glEnd();
+  drawBall();
+ 
+  rtri += 0.005f;                    // Increase The Rotation Variable For The Triangle
+  if(rtri>2)
+      rtri = -2.0f;
+  rquad -= 15.0f;                    // Decrease The Rotation Variable For The Quad
+
+  // swap the buffers to display, since double buffering is used.
   glutSwapBuffers();
 }
-static void key(unsigned char key, int x, int y)
+
+
+void keyPressed(unsigned char key, int x, int y) 
 {
-    switch (key)
-    {
-        case 27 :
-        case 'q':
-            exit(0);
-            break;
-        case '+':
-            slices++;
-            stacks++;
-            break;
-        case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
-            break;
+    if (key == 'q') 
+    { 
+		glutDestroyWindow(window);
+		exit(0);                  
     }
-    glutPostRedisplay();
 }
-static void idle(void)
+
+void InitGL(int Width, int Height)         
 {
-    glutPostRedisplay();
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);     
+  glClearDepth(1.0);
+  glDepthFunc(GL_LESS);
+  glEnable(GL_DEPTH_TEST);
+  glShadeModel(GL_SMOOTH);
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity(); 
+
+  gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,100.0f);  
+
+  glMatrixMode(GL_MODELVIEW);
 }
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
 
-int main(int argc, char *argv[])
-{
-    glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("Animation (Spinnig Sphere with Disco Lighting)");
-
-    glutReshapeFunc(resize);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(key);
-    glutIdleFunc(idle);
-
-    glClearColor(0,0,0,0);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHTING);
-    glEnable(GL_FLAT);
-    glShadeModel(GL_FLAT);
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-
-    glutMainLoop();
-
-    return EXIT_SUCCESS;
+int main(int argc, char **argv) 
+{  
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
+  glutInitWindowSize(640, 480);
+  glutInitWindowPosition(0, 0);
+  window = glutCreateWindow("Moving Car");
+  glutDisplayFunc(&DrawGLScene);
+  glutIdleFunc(&DrawGLScene);
+  glutKeyboardFunc(&keyPressed);
+  InitGL(640, 480);
+  glutMainLoop();
+  return 1;
 }
