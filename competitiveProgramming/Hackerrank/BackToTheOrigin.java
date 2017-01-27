8d64e14dc3d95cdf6e7758ee6b16b0c490ab6231
Samarth Sanghi <sanghi.samarth@gmail.com>
2017-01-27 19:31:45 +0530

    CodeAgon#1

---
+++ competitiveProgramming/Hackerrank/BackToTheOrigin.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        long xT = in.nextLong();
+        long yT = in.nextLong();
+        int n = in.nextInt();
+        long[][] direction = new long[n][2];
+        for(int i=0; i<n; i++)
+            for(int j=0; j<2; j++)
+                direction[i][j] = in.nextLong();
+        long xV = xT;
+        long yV = yT;
+        for(int i=n-1; i>=0; i--)
+        {
+            xV -= direction[i][0];
+            yV -= direction[i][1];
+        }
+        System.out.println(xV + " " + yV);
+        in.close();
+    }
+}
